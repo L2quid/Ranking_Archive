bfce503a6fd8b4b8d00a8e00f50c9bf9898084c2
L2quid <wodnd101@naver.com>
2019-08-19 23:33:15 +0900

    크롤링 프로그램수정
    초기 페이지 수정 및 파일명 변경, CSS파일추가, 크롤링 프로그램 수정

--- 크롤링프로그램/리디북스 순위.py
+++ 크롤링프로그램/리디북스 순위.py
@@ -9,9 +9,12 @@ with urllib.request.urlopen(url) as fs :
     items = soup.find_all('a', {'class' : 'title_link'})
     price=soup.find_all('span',{'class':'price'})
     name=soup.find_all('p',{'class':'book_metadata author'})
+    images=[]
+    for img in soup.findAll('img'):
+        images.append(img.get('data-src'))
 date= datetime.datetime.today().strftime('%Y%m%d')
 desktopPath = os.path.expanduser('~')
 filePath = desktopPath +'/Documents/GitHub/Ranking_Archive/data/book/'+str(date)+'.txt'
 with open( filePath, 'w+',encoding='utf-8') as file:
     for i in range(20) :
-        file.write('<div class="rank">'+str(i + 1) + '위</div><div class="title"> ' + items[i+1].get_text(strip = True)+'</div><div class="title">'+name[i+1].get_text(strip = True)+'</div><div class="title">'+price[i+1].get_text(strip = True)+'</div><br>')
+        file.write('<div class="wrap"><div class="rank">'+str(i + 1) + '위</div><img src="'+images[i+1]+'"><div class="title"> ' + items[i+1].get_text(strip = True)+'</div><div class="title">'+name[i+1].get_text(strip = True)+'</div><div class="title">'+price[i+1].get_text(strip = True)+'</div></div><br>')
---
+++ 크롤링프로그램/벅스 음악순위.py
@@ -0,0 +1,18 @@
+import os
+import datetime
+from bs4 import BeautifulSoup
+import urllib.request
+date= datetime.datetime.today().strftime('%Y%m%d')
+url = 'https://music.bugs.co.kr/chart/track/realtime/total?chartdate='+date
+with urllib.request.urlopen(url) as fs :
+    soup = BeautifulSoup(fs.read().decode(fs.headers.get_content_charset()), 'html.parser')
+    items = soup.find_all('p', {'class' : 'title'})
+    names=soup.find_all('p', {'class' : 'artist'})
+    images=[]
+    for img in soup.findAll('img',src=True):
+        images.append(img.get('src'))
+desktopPath = os.path.expanduser('~')
+filePath = desktopPath + '/Documents/GitHub/Ranking_Archive/data/music/'+str(date)+'.txt'
+with open( filePath, 'w+',encoding='utf-8') as file:
+    for i in range(20) :
+        file.write('<div class="rank">'+str(i + 1) + '위</div><img src="'+str(images[i+17])+'"><div class="title">' + items[i].get_text(strip = True)+'</div><div class="title">'+ names[i].get_text(strip = True)+ '</div><br>')
