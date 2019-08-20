6f839d5470d928f15ba6996a18a3c995a2260114
L2quid <wodnd101@naver.com>
2019-08-20 21:07:31 +0900

    파일복구
    네이버 영화순위 파일 복구

---
+++ 크롤링프로그램/네이버영화순위.py
@@ -0,0 +1,18 @@
+import os
+import datetime
+from bs4 import BeautifulSoup
+import urllib.request
+
+url = 'https://movie.naver.com/movie/running/current.nhn'
+with urllib.request.urlopen(url) as fs :
+    soup = BeautifulSoup(fs.read().decode(fs.headers.get_content_charset()), 'html.parser')
+    items = soup.find_all('dt', {'class' : 'tit'})
+    images=[]
+    for img in soup.findAll('img'):
+        images.append(img.get('src'))
+date= datetime.datetime.today().strftime('%Y%m%d')
+desktopPath = os.path.expanduser('~')
+filePath = desktopPath + '/Documents/GitHub/Ranking_Archive/data/movie/'+str(date)+'.txt'
+with open( filePath, 'w+',encoding='utf-8') as file:
+    for i in range(20) :
+        file.write('<div class="rank">'+str(i + 1) + '위</div><img src="'+images[i+74]+'"><div class="title"> ' +items[i].contents[3].get_text(strip = True)+'</div><br>')
